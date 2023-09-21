86b617ede7dff03c67361506f5c6dcdbfa77d18b
Éric Alonso Martínez <113835039+eralonso@users.noreply.github.com>
2023-09-21 19:28:42 +0200

    CPP06 - [ ex00 - Remember that is mandatory store value in that correspondent type and after cast it to the other three types ]

--- ex00/inc/ScalarConverter.hpp
+++ ex00/inc/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: eralonso <eralonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 12:42:28 by eralonso          #+#    #+#             */
-/*   Updated: 2023/09/21 13:29:21 by eralonso         ###   ########.fr       */
+/*   Updated: 2023/09/21 16:06:54 by eralonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,37 @@
 #define TYPE_DOUBLE 3
 #define TYPE_INVALID ( NUM_TYPES - 1 )
 
+#define MINUS_INFF_LIMIT 0
+#define PLUS_INFF_LIMIT 1
+#define NANF_LIMIT 2
+#define MINUS_INF_LIMIT 3
+#define PLUS_INF_LIMIT 4
+#define NAN_LIMIT 5
+#define NOT_LIMIT 6
+
+#define	LIMITS_FLOAT 3
+#define	LIMITS_DOUBLE 7
+
 #define ERR_MSG_INVALID_TYPE "Invalid type of literal\n\tAllowed literals are: [ Char -> 'a' ] | [ Int -> 1 ] | [ Float -> 1.5f ] | [ Double -> 1.5 ]"
 
 class ScalarConverter
 {
 	private:
+		
 		static int8_t		getType( const std::string& str );
 		static std::string	itos( int num );
+		static int8_t		checkInfNan( const std::string& str, int8_t type );
+		static bool			checkCharLiteral( const std::string& str );
+		static bool			checkIsChar( const std::string& str, int ( *f )( int ) );
 		static bool			isCharType( const std::string& str );
 		static bool			isIntType( const std::string& str );
 		static bool			isFloatType( const std::string& str );
 		static bool			isDoubleType( const std::string& str );
 		static bool			isInvalidType( const std::string& str );
+		static void			printChar( const std::string& str );
+		static void			printInt( const std::string& str );
+		static void			printFloat( const std::string& str );
+		static void			printDouble( const std::string& str );
 	public:
 		ScalarConverter( void );
 		~ScalarConverter( void );
--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: eralonso <eralonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 12:42:28 by eralonso          #+#    #+#             */
-/*   Updated: 2023/09/21 13:36:01 by eralonso         ###   ########.fr       */
+/*   Updated: 2023/09/21 19:27:05 by eralonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,38 +37,101 @@ std::string	ScalarConverter::itos( int num )
 	return ( str );
 }
 
+int8_t	ScalarConverter::checkInfNan( const std::string& str, int8_t type )
+{
+	int8_t		limit;
+	std::string	arr[ 6 ] = { \
+							"-inff", "+inff", "nanf", \
+							"-inf", "+inf", "nan" };
+	int8_t		types[ 8 ] = { \
+								MINUS_INFF_LIMIT, PLUS_INFF_LIMIT, NANF_LIMIT, \
+								NOT_LIMIT, \
+								MINUS_INF_LIMIT, PLUS_INF_LIMIT, NAN_LIMIT, \
+								NOT_LIMIT };
+	
+	for ( limit = type - 3; limit < type && str.compare( arr[ limit ] ); limit++ )
+		;
+	return ( types[ limit ] );
+}
+
 bool	ScalarConverter::isDoubleType( const std::string& str )
 {
+	std::string::size_type	i;
+	int8_t					dot;
+
+	if ( checkInfNan( str, LIMITS_DOUBLE ) != NOT_LIMIT )
+		return ( true );
+	i = ( str[ 0 ] == '-' || str[ 0 ] == '+' ) ? 1 : 0;
+	dot = 0;
+	for ( ; i < str.length(); i++ )
+	{
+		if ( std::isdigit( str[ i ] ) == 0 || str[ i ] == '.' )
+			return ( false );
+		if ( str[ i ] == '.' && dot )
+			return ( false );
+		else if ( str[ i ] == '.' )
+			dot = 1;
+	}
+	if ( dot == 0 || str.length() < 2 )
+		return ( false );
+	return ( true );
 }
 
 bool	ScalarConverter::isFloatType( const std::string& str )
 {
+	std::string::size_type	i;
+	int8_t					dot;
+
+	if ( checkInfNan( str, LIMITS_FLOAT ) != NOT_LIMIT )
+		return ( true );
+	i = ( str[ 0 ] == '-' || str[ 0 ] == '+' ) ? 1 : 0;
+	dot = 0;
+	for ( ; i < str.length() && str[ i ] != 'f'; i++ )
+	{
+		if ( std::isdigit( str[ i ] ) == 0 || str[ i ] == '.' )
+			return ( false );
+		if ( str[ i ] == '.' && dot )
+			return ( false );
+		else if ( str[ i ] == '.' )
+			dot = 1;
+	}
+	if ( str[ i ] != 'f' || str[ i + 1 ] != '\0' || dot == 0 || str.length() < 3 )
+		return ( false );
+	return ( true );
 }
 
 bool	ScalarConverter::isIntType( const std::string& str )
 {
 	std::string::size_type	i;
-	int8_t					info;
-	std::string				max_int = itos( std::numeric_limits<int>::max() );
-	std::string				min_int = itos( std::numeric_limits<int>::min() );
 
 	i = ( str[ 0 ] == '-' || str[ 0 ] == '+' ) ? 1 : 0;
-	info = ( str[ 0 ] == '-' || str[ 0 ] == '+' ) ? 1 : 0;
-	info |= ( str[ 0 ] == '-' ? 1 : 0 ) << 1;
 	for ( ; i < str.length(); i++ )
 		if ( std::isdigit( str[ i ] ) == 0 )
 			return ( false );
-	if ( i - ( info & 1 ) > max_int.length() )
 	return (true);
+}
+
+bool	ScalarConverter::checkIsChar( const std::string& str, int ( *f )( int ) )
+{
+	int	value;
+
+	if ( str.length() > 0 && str.length() < 4 && isIntType( str ) )
+	{
+		value = std::stoi( str );
+		return ( f( value ) );
+	}
+	return ( false );
+}
+
+bool	ScalarConverter::checkCharLiteral( const std::string& str )
+{
+	return ( ( str.length() == 3 && str[ 0 ] == '\'' && str[ 2 ] == '\'' ) || \
+			 ( str.length() == 1 &&  ) );
 }
 
 bool	ScalarConverter::isCharType( const std::string& str )
 {
-	if ( str.length() == 3 && str[ 0 ] == '\'' && str[ 2 ] == '\'' && isprint( str[ 1 ] ) )
-		return ( true );
-	else if ( str.length() == 1 && isprint( str[ 0 ] ) )
-		return ( true );
-	return ( false );
+	return ( checkCharLiteral( str ) );
 }
 
 int8_t	ScalarConverter::getType( const std::string& str )
@@ -88,6 +151,11 @@ int8_t	ScalarConverter::getType( const std::string& str )
 void	ScalarConverter::convert( const std::string& toConvert )
 {
 	int8_t	type;
+	void	( *print[ NUM_TYPES - 1 ] )( const std::string& ) = { \
+										&ScalarConverter::printChar, \
+										&ScalarConverter::printInt, \
+										&ScalarConverter::printFloat, \
+										&ScalarConverter::printDouble };
 
 	type = getType( toConvert );
 	if ( type == TYPE_INVALID )
@@ -95,4 +163,30 @@ void	ScalarConverter::convert( const std::string& toConvert )
 		std::cerr << ERR_MSG_INVALID_TYPE << std::endl;
 		return ;
 	}
+}
+
+void	ScalarConverter::printChar( const std::string& str )
+{
+	std::string	print;
+
+	if ( str.length() == 3 && str[ 0 ] == '\'' && str[ 2 ] == '\'' )
+		std::cout << str[ 1 ] << std::endl;
+	else if ( str.length() == 1 && isdigit( str[ 0 ] ) &&  )
+		std::cout << str[ 1 ] << std::endl;
+	std::cout << str << std::endl;
+}
+
+void	ScalarConverter::printInt( const std::string& str )
+{
+	std::cout << str << std::endl;
+}
+
+void	ScalarConverter::printFloat( const std::string& str )
+{
+	std::cout << str << std::endl;
+}
+
+void	ScalarConverter::printDouble( const std::string& str )
+{
+	std::cout << str << std::endl;
 }
